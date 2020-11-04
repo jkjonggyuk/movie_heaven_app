1e9c26225b2875175ffc5e44dd9b4fca1b03f377
JK Kim <jk.jonggyuk@gmail.com>
2020-11-03 22:41:28 -0800

    Updates

---
+++ precache-manifest.4dabb1564572207199baaceb8b023b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426cc1100171d6911903ea4cd9028fa2",
+    "url": "/movie_heaven_app/index.html"
+  },
+  {
+    "revision": "5dabdb62d3278f144906",
+    "url": "/movie_heaven_app/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "3eb26b5b0d51897434c0",
+    "url": "/movie_heaven_app/static/js/2.daa93c6e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/movie_heaven_app/static/js/2.daa93c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dabdb62d3278f144906",
+    "url": "/movie_heaven_app/static/js/main.6b0468f4.chunk.js"
+  },
+  {
+    "revision": "749c5818936478879158",
+    "url": "/movie_heaven_app/static/js/runtime-main.645ad829.js"
+  }
+]);
